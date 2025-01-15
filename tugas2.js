ae0fdc58f43f5af11da90b0c24b5204feb284e5b
nurlintang01 <nurlintang395@gmail.com>
2025-01-15 11:18:23 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,18 @@
+let namaDepan = " Nur Lintang";
+let namaTengah = " Khofifah";
+let namaBelakang = " Azzahra";
+let alamat = " Rusunawa baru karangroto";
+let usia = " 16 Tahun";
+let ttl = " Semarang, 04 Januari 2009";
+let gender = " Peempuan";
+let agama = " Islam";
+let email = " nurlintang25@gmail.com";
+let berat = " 60 kg";
+let tinggi = " 160 cm"
+let hobi = " membaca novel";
+let cita = " Hukum";
+let pekerjaan = " pelajar";
+let sekolah = " SMKN 1 SAYUNG";
+let jurusan = " PPLG";
+
+document.write("nama saya adalah " + namaDepan + " " + namaTengah + " " + namaBelakang + " " + "saya tinggal di " + alamat + " pada tahun 2025 usia saya  " + usia + " saya lahir di " + ttl + " jenis kelamin saya adalah " + gender + " saya beragama " + agama + " alamat email saya adalah" + " email" + " saya memiliki berat badan" + berat + " dengan tinggi" + tinggi + " saya sangat senang" + hobi + " karena saya memiliki cita-cita" + cita + " saat ini saya adalah seorang" + pekerjaan + " yang bersekolah di" + sekolah + " pada jurusan" + jurusan);
